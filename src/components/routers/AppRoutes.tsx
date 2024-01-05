ab0a42114b0f0e0dc6c9f2b1708addf579fc1d6e
Narges Jahromi <nrgs.jahromi@gmail.com>
2024-01-05 20:32:52 +0330

    add tailwind

---
+++ src/components/routers/AppRoutes.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { FC } from "react";
+// import Login from "../auth/Login";
+// import Signup from "../auth/Signup";
+import Auth from "../auth/Auth";
+
+const AppRoutes: FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        {/* <Route path="/signup" element={<Signup />} /> */}
+
+        {/* <Route path="/app" element={<Dashboard />}>
+          <Route path="entity/:entityId/instances" element={<InstanceListPage />} />
+          <Route path="entity/:entityId/instances/owned" element={<OwnedInstanceListPage />} />
+          <Route path="entity/:entityId/instances/create" element={<InstanceCreatePage />} />
+          <Route path="entity/:entityId/instances/:instanceId" element={<InstanceDetailsPage />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/app" />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
